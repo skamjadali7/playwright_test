0b8b28c64334d121843dc0fa34252da21fd3042d
SK Amjad Ali <amjadali@SKs-Laptop.local>
2024-06-13 23:07:01 +0530

    13June2024

---
+++ tests/waitMechanism.spec.ts
@@ -0,0 +1,41 @@
+import {expect, test} from '@playwright/test'
+
+test.beforeEach(async({page})=>{
+    await page.goto('http://uitestingplayground.com/ajax')
+})
+
+test('auto wait mechanisim with waitFor state',async({page})=>{
+    await page.locator('#ajaxButton').click()
+    const successText = page.locator('.bg-success')
+     // waiting as per state
+    await successText.waitFor({state:"attached"})
+   await expect(successText).toHaveText('Data loaded with AJAX get request.')
+})
+
+test('auto wait mechanisim with default timeout',async({page})=>{
+    await page.locator('#ajaxButton').click()
+    const successText = page.locator('.bg-success')
+    
+    await expect(successText).toHaveText('Data loaded with AJAX get request.',{timeout:10000})
+})
+
+test('alternative wait with element',async({page})=>{
+    await page.locator('#ajaxButton').click()
+    const successText = page.locator('.bg-success')
+    await page.waitForSelector('.bg-success')
+    await expect(successText).toHaveText('Data loaded with AJAX get request.')
+})
+
+test('alternative wait for response',async({page})=>{
+    await page.locator('#ajaxButton').click()
+    const successText = page.locator('.bg-success')
+    await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
+    await expect(successText).toHaveText('Data loaded with AJAX get request.')
+})
+
+test('alternative wait for network call completed',async({page})=>{  // not ideal but we can check 
+    await page.locator('#ajaxButton').click()
+    const successText = page.locator('.bg-success')
+    await page.waitForLoadState('networkidle')
+    await expect(successText).toHaveText('Data loaded with AJAX get request.')
+})
