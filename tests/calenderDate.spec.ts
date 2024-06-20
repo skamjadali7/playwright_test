fe751121ae9e0ddb8af4fde97d3bc10a3cacf60e
SK Amjad Ali <amjadali@SKs-Laptop.local>
2024-06-21 00:22:29 +0530

    21June2024

--- tests/calenderDate.spec.ts
+++ tests/calenderDate.spec.ts
@@ -16,3 +16,27 @@ test('select Days from current Month {common datepicker}',async({page})=>{
     //Assertion
     expect(commonDatePicker).toHaveValue('Jun 1, 2024')
 })
+
+test('customize calender selection',async({page})=>{
+    const commonDatePicket = await page.getByPlaceholder('Form Picker')
+    commonDatePicket.click()
+
+    let date = new Date()
+    date.setDate(date.getDate()+7)
+    const expectedDate = date.getDate().toString()
+    const expectedMonthShort = date.toLocaleString('En-US',{month:'short'})
+    const expectedMonthLong = date.toLocaleString('En-US',{month:'long'})
+    const expectedYear = date.getFullYear()
+    const datetoAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`
+
+    let calenderMonthAndYear = await page.locator('nb-calendar-view-mode button').textContent()
+    const expectedMonthAndYear = `${expectedMonthLong} ${expectedYear}`
+
+    while(!calenderMonthAndYear?.includes(expectedMonthAndYear)){
+        await page.locator('button.next-month').click()  // for future date selection
+        calenderMonthAndYear = await page.locator('nb-calendar-view-mode button').textContent()
+    }
+
+    await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate,{exact:true}).click()
+    await expect(commonDatePicket).toHaveValue(datetoAssert)
+})
